946006f6ce7eb6ce3bc7c889d12ad1199556b234
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-04-04 11:54:37 -0500

    04-funcioones

---
+++ arit.cpp
@@ -0,0 +1,14 @@
+#include "arit.h"
+
+// En el .cpp va la implementacion de mis funciones
+int suma(int a, int b){
+    return a + b;
+}
+
+int resta(int a, int b){
+    return a - b;
+}
+
+int producto(int a, int b){
+    return a * b;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <iostream> // cout 
+#include "arit.h"   // suma, resta
 
-// g++ -std=c++2b main.cpp -o main
+// g++ -std=c++2b main.cpp arit.cpp -o main
 // El main() debe ser muy pero muy pequeñito
 int main() {
     std::cout << "Hello FundProg-UNI!" << std::endl;
+    
+    int a = 50;
+    int b = 20;
+    int c = suma(a, b);
+    std::cout << c << std::endl;
+    //std::cout << "La suma de " << a << " y " << b << " es " << c << std::endl;
+    c = resta(a, b);
+    std::cout << c << std::endl;
 
+    c = producto(a, b);
+    std::cout << c << std::endl;
     return 0;
 }
