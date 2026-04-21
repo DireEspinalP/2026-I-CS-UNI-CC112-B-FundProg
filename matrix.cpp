d75409e9e0a4f7bbb9c85824393fde78b726eae8
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-04-21 16:57:51 -0500

    Tarea de Matrix

--- matrix.cpp
+++ matrix.cpp
@@ -2,20 +2,59 @@
 #include <iostream>
 
 
-using namespace std;
 
+using namespace std;
+     void CreateMatrix(TP **&pMat, size_t rows, size_t    cols){
+    pMat = new TP*[rows];
+    for (size_t i = 0; i < rows; ++i){
+        pMat[i] = new TP[cols];
+    }
+     }
+
+     
+
+     void ReadMatrix(TP **pMat, size_t rows, size_t cols){
+    for (size_t i = 0; i < rows; ++i){
+        for (size_t  j = 0; j < cols; ++j)
+            cin >> pMat[i][j];
+    }
+}
+     
+
+     // PrintMatrix(pMat, rows, cols);
+     void PrintMatrix(TP **pMat, size_t rows, size_t cols){
+        for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j){
+            cout << pMat[i][j] << " ";  
+         
+             }
+    cout << endl;
+            }     
+
+     }  
+     
+     void DeleteMatrix(TP **&pMat, size_t rows){
+    for (size_t i = 0; i < rows; ++i){
+        delete[] pMat[i];
+    delete[] pMat;
+    pMat = nullptr;
+    }
+     }
 void DemoMatrix1(){
-    size_t rows, cols;
+    TP **pMat = nullptr;
+    size_t rows=0, cols=0;
     // TP **pMat = nullptr;
 
-    cout << "Demostracion de matrizes" << endl;
-    cout << "Ingrese nro de filas: ";
-    cin >> rows;
-    cout << "Ingrese nro de columnas: ";
+   
+    cout << "Ingrese el numero de filas : ";
+    cin >> rows ;
+    cout << "Ingrese el numero de columnas: ";    
     cin >> cols;
-    
-    // CreateMatrix(pMat, rows, cols);
-    // ReadMatrix(pMat, rows, cols);
-    // PrintMatrix(pMat, rows, cols);
-    // DeleteMatrix(pMat, rows);
+   CreateMatrix(pMat, rows, cols);
+  cout << "Ingrese los elementos de la matriz: " << endl;
+   ReadMatrix(pMat, rows, cols);   
+ cout << "La matriz ingresada es: " << endl;
+    PrintMatrix(pMat, rows, cols);
+  cout << endl;
+    DeleteMatrix(pMat, rows);
 }
--- matrix.h
+++ matrix.h
@@ -1,8 +1,12 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
-
+#include <iostream>
 #include "types.h"
-
+using namespace std;
+     void CreateMatrix(TP **&pMat, size_t rows, size_t cols);
+     void ReadMatrix(TP **pMat, size_t rows, size_t cols);
+     void PrintMatrix(TP **pMat, size_t rows, size_t cols);
+     void DeleteMatrix(TP **&pMat, size_t rows);
 
 void DemoMatrix1();
 
